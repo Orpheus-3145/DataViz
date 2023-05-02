6bd75b80d9381660668c7e32fb3c8c1f4eb26356
Francesco Aru <faru@codam.student.nl>
2023-05-02 18:18:05 +0200

    added data links

---
+++ main.py
@@ -0,0 +1,9 @@
+# Dataset links:
+#   - https://www.kaggle.com/datasets/thedevastator/impact-of-living-standards-on-dry-forest-degrada
+#   - https://www.kaggle.com/datasets/konradb/eubucco-european-buildings-dataset
+#   - kaggle: https://www.kaggle.com/datasets
+#   - https://catalog.data.gov/dataset/national-flood-hazard-layer-nfhl
+
+
+if __name__ == "__main__":
+    print("Hello world!")
